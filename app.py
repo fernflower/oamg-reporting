727005cf86ca68a31e42956cdcb3b60d24b3c13c
Inessa Vasilevskaya <ivasilev@redhat.com>
2019-06-28 23:53:06 +0200

    initial: validation introduced

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import sys
 
 
 TOPIC=os.environ.get("QUEUE", "platform.upload.leapp-reporting")
+VALIDATION_TOPIC=os.environ.get("RESPONSE_QUEUE", "platform.upload.validation")
 BOOT_SERVERS=os.environ.get("KAFKAMQ", "platform-mq-ci-kafka-bootstrap.platform-mq-ci.svc:9092").split(',')
 LOG = logging.getLogger()
 
@@ -18,14 +19,17 @@ def fetch_report(url):
     res = []
     req = requests.get(url)
     if not req.ok:
-        LOG.warn("[{}] Error during GET {}".format(req.status_code, url))
+        LOG.warn("[%s] Error during GET %s", req.status_code, url)
     else:
         report = io.BytesIO(req.content)
-        tar = tarfile.open(fileobj=report)
-        for report in tar:
-            f = tar.extractfile(report)
-            json_report = json.loads(f.read())
-            res.append(json_report)
+        try:
+            tar = tarfile.open(fileobj=report)
+            for report in tar:
+                f = tar.extractfile(report)
+                json_report = json.loads(f.read())
+                res.append(json_report)
+        except (tarfile.ReadError, json.decoder.JSONDecodeError) as e:
+            LOG.warn("Bad payload: %s", str(e))
     return res
 
 
@@ -36,21 +40,36 @@ def _deserializer(m):
         return {}
 
 
-def main():
-    consumer = kafka.KafkaConsumer(TOPIC,
-                                   bootstrap_servers=BOOT_SERVERS,
+def main(consumer_topic=TOPIC, producer_topic=VALIDATION_TOPIC, boot_servers=BOOT_SERVERS):
+    consumer = kafka.KafkaConsumer(consumer_topic,
+                                   bootstrap_servers=boot_servers,
                                    auto_offset_reset="earliest",
                                    value_deserializer=_deserializer)
+    producer = kafka.KafkaProducer(bootstrap_servers=boot_servers,
+                                   value_serializer=lambda m: json.dumps(m).encode('utf-8'))
+
+    def validate_data(request_id, result="success"):
+        msg = {"request_id": request_id, "validation": result}
+        producer.send(producer_topic, msg)
+
     for msg in consumer:
         data = msg.value
         if not data or not isinstance(data, dict):
+            # smth is wrong, not processing
             continue
         reports = fetch_report(data["url"])
-        # XXX FIXME POC
-        sys.stdout.write("====================\n")
-        for report in reports:
-            sys.stdout.write("Received: %s\n" % report)
-        sys.stdout.write("====================\n")
+        if not reports:
+            # couldn't fetch report data, discarding message
+            validate_data(data.get("request_id"), result="handoff")
+        else:
+            # data is valid, waving green flag
+            validate_data(data.get("request_id"))
+            # process data (upload to RDS in our case tbd)
+            # XXX FIXME POC
+            sys.stdout.write("====================\n")
+            for report in reports:
+                sys.stdout.write("Received: %s\n" % report)
+            sys.stdout.write("====================\n")
 
 
 if __name__ == "__main__":
